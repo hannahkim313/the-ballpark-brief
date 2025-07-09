43d327a860bcc84bc508dd3886030b12bc4a0da1
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-07-09 09:12:54 -0700

    Add date picker and show empty game overview state

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,37 @@
 const Page = () => {
-  return <h1>Home Page</h1>;
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  };
+
+  const today = new Date();
+  const todaysFullDate = today.toLocaleDateString(undefined, dateOptions);
+  const maxDate = today.toISOString().split('T')[0];
+
+  return (
+    <div>
+      <section>
+        <h1>The Ballpark Brief: Game Insights, Lineups & More</h1>
+        <p>Today&apos;s date: {todaysFullDate}</p>
+        <label htmlFor="gameDate">Game date:</label>
+        <input
+          type="date"
+          id="gameDate"
+          name="gameDate"
+          value={maxDate}
+          min="2020-03-30"
+          max={maxDate}
+        />
+      </section>
+      <section>
+        <h2>Game Overview</h2>
+        <p>No game on deck!</p>
+        <p>Select a date above to view available games for that day.</p>
+      </section>
+    </div>
+  );
 };
 
 export default Page;
