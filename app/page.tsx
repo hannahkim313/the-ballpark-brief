b9bc13989756825f5a5676ddb746e45f7b31aec3
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-07-29 21:25:20 -0700

    Create a tab navigation for displaying different game data

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
-import GameOverview from '@/components/GameOverview';
 import GameSelector from '@/components/GameSelector';
+import GameOverview from '@/components/GameOverview';
+import GameTabs from '@/components/GameTabs';
 
 const Page = () => {
   return (
     <div>
       <GameSelector />
-
       <GameOverview />
-
-      <section>
-        <h2>Game Overview</h2>
-        <p>No game on deck!</p>
-        <p>Select a date above to view available games for that day.</p>
-      </section>
+      <GameTabs />
     </div>
   );
 };
---
+++ components/GameTabs.tsx
@@ -0,0 +1,29 @@
+const GameTabs = () => {
+  return (
+    <section>
+      <h2>Game Details</h2>
+
+      <nav>
+        <ul role="tablist">
+          <li role="presentation">
+            <button role="tab" aria-selected="true">
+              General
+            </button>
+          </li>
+          <li role="presentation">
+            <button role="tab" aria-selected="false">
+              Lineups
+            </button>
+          </li>
+          <li role="presentation">
+            <button role="tab" aria-selected="false">
+              Box
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
+};
+
+export default GameTabs;
