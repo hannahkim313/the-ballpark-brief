e1a256c381cb04644407b38cd09fc713ffa8f64b
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-08-12 16:27:43 -0700

    Remove today's date from the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,7 @@
 const Header = () => {
-  const dateOptions: Intl.DateTimeFormatOptions = {
-    weekday: 'long',
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
-  };
-
-  const today = new Date();
-  const todaysFullDate = today.toLocaleDateString(undefined, dateOptions);
-
   return (
-    <header className="px-content py-content">
-      <h1>The Ballpark Brief: Game Insights, Lineups & More</h1>
-      <p>{todaysFullDate}</p>
+    <header className="px-content py-content text-center shadow-md">
+      <h1 className="mb-0">The Ballpark Brief</h1>
     </header>
   );
 };
