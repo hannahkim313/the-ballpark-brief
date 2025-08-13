f815b5fea4f335600c4ded6e9c6c98a89dd85d21
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-08-13 12:28:27 -0700

    Increase font size of team names in game overview at md breakpoint

--- components/GameOverview.tsx
+++ components/GameOverview.tsx
@@ -2,12 +2,13 @@ const GameOverview = () => {
   return (
     <section className="px-content py-content bg-stone-100 text-center">
       <h2>Game Overview</h2>
+
       {/* TODO: Replace content with dynamic values from API */}
       <div className="space-y-4">
         <p className="inline-flex items-center gap-4">
           <span className="inline-flex flex-col whitespace-nowrap md:w-xs">
             <span className="text-lg font-semibold md:hidden">LAA</span>
-            <span className="hidden text-lg font-semibold md:inline">
+            <span className="hidden text-lg font-semibold md:inline lg:text-xl">
               Los Angeles Angels
             </span>
             <span>(55-49)</span>
@@ -15,16 +16,18 @@ const GameOverview = () => {
           @
           <span className="inline-flex flex-col whitespace-nowrap md:w-xs">
             <span className="text-lg font-semibold md:hidden">SEA</span>
-            <span className="hidden text-lg font-semibold md:inline">
+            <span className="hidden text-lg font-semibold md:inline lg:text-xl">
               Seattle Mariners
             </span>
             <span>(55-49)</span>
           </span>
         </p>
+
         <div>
           <p>July 25, 2025</p>
           <p>6:40 PM PDT</p>
         </div>
+
         <p>T-Mobile Park in Seattle, WA</p>
       </div>
     </section>
