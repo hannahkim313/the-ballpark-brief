b66da4d7f73fe05798789104f727dd885c7af272
Hannah <67137839+hannahkim313@users.noreply.github.com>
2025-08-25 12:31:48 -0700

    Create a game response type to shape game-related data

--- types/statsAPI.ts
+++ types/statsAPI.ts
@@ -18,3 +18,66 @@ export type ScheduleResponse = {
     }[];
   }[];
 };
+
+export type GameResponse = {
+  gameData: {
+    datetime: {
+      ampm: string;
+      dateTime: string;
+      time: string;
+    };
+    gameInfo: {
+      attendance: number;
+      firstPitch: string;
+      gameDurationMinutes: number;
+    };
+    teams: {
+      away: {
+        name: string;
+        record: {
+          leagueRecord: {
+            losses: number;
+            wins: number;
+          };
+        };
+      };
+      home: {
+        name: string;
+        record: {
+          leagueRecord: {
+            losses: number;
+            wins: number;
+          };
+        };
+      };
+    };
+    status: {
+      abstractGameState: string;
+    };
+    venue: {
+      location: {
+        city: string;
+        stateAbbrev: string;
+      };
+      name: string;
+      timeZone: {
+        tz: string;
+      };
+    };
+    weather: {
+      condition: string;
+      temp: string;
+      wind: string;
+    };
+  };
+  liveData: {
+    boxscore: {
+      officials: {
+        official: {
+          fullName: string;
+        };
+        officialType: string;
+      }[];
+    };
+  };
+};
